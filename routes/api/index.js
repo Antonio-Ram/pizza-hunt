a5605520523c85e532cd41c7c2d0bf5b85420be4
Antonio <tonito@umich.edu>
2022-02-04 17:36:59 -0500

    Solved Error

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
-router.route('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 // add prefix of `./pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
 
